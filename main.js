20907805b54a6964700f057bb8d93f7dc14da749
Prabhu V <prabhuofficial2003@gmail.com>
2024-02-24 14:56:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ menu.addEventListener('click', ()=>{
 function menuAnimation(x) {
   x.classList.toggle('change');
 }
+
+window.onscroll = function()
+{topbar()};
+
+function topbar() {
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topbar").style.top = "0";
+  } else {
+    document.getElementById("topbar").style.top = "-100px";
+  }
+}
